a51a87ed611ec4f715bf0fb594a3d9a1a9f85d38
Art <artursudresecundaria@gmail.com>
2025-09-01 16:40:49 -0300

    feature: login, authenticação de usuario no bd.

--- config/usuarios/urls.py
+++ config/usuarios/urls.py
@@ -1,9 +1,8 @@
 # usuarios/urls.py
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
     
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='usuarios/login.html'), name='login'),
+    path('login/', views.login_view, name='login'),
 ]
