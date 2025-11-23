cf1733b23cb5e938f29da747d3fdb70a4d825bda
Rafael <rafa0633@gmail.com>
2025-11-23 19:58:29 -0300

    Adicionando API views e urls

---
+++ config/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import (
+    AreaListView,
+    TrilhaListCreateView,
+    TrilhaDetailView
+)
+
+urlpatterns = [
+    path("areas/", AreaListView.as_view(), name="lista-areas"),
+    path("trilhas/", TrilhaListCreateView.as_view(), name="lista-trilhas"),
+    path("trilhas/<int:pk>/", TrilhaDetailView.as_view(), name="detalhe-trilha"),
+]
