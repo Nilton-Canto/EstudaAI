ad0bef8aedd375ab3a2ee8965719fb8aaa09da99
Seu Nome <seu-email@exemplo.com>
2025-11-23 18:24:19 -0300

    style: format usuarios/urls.py with black

--- config/usuarios/urls.py
+++ config/usuarios/urls.py
@@ -64,7 +64,6 @@ urlpatterns = [
     ),
     # URLs de chat IA
     path("chat-ia/", views.chat_ia, name="chat_ia"),
-
     # URLs de trilhas do usuário
     path("minhastrilhas/", views.minhas_trilhas, name="minhas_trilhas"),
     path("criartrilha/", views.criar_trilha, name="criar_trilha"),
